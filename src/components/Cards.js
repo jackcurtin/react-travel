a9af6c3c4011ca2b22f18ecb0f8b03e8c1beccec
Jack Curtin <jackcurtin94@gmail.com>
2021-06-16 16:15:23 -0500

    filled out cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardItem from "./CardItem";
+import "./Cards.css"
 
 function Cards(props) {
     return (
@@ -8,7 +9,38 @@ function Cards(props) {
             <div className={"cards__container"}>
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem
+                        src="./images/img-9.jpg"
+                        text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                        label="Adventure"
+                        path="/services"
+                        />
+                        <CardItem
+                            src="./images/img-2.jpg"
+                            text="Travel through the Islands of Bali in a Private Cruise"
+                            label="Luxury"
+                            path="/services"
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem
+                            src="./images/img-3.jpg"
+                            text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+                            label="Mystery"
+                            path="/services"
+                        />
+                        <CardItem
+                            src="./images/img-4.jpg"
+                            text="Play a pick-up game on the Most Remote Soccer Field in the World"
+                            label="Adrenaline"
+                            path="/services"
+                        />
+                        <CardItem
+                            src="./images/img-8.jpg"
+                            text="Ruide through the Sahara Desert on a guided camel tour"
+                            label="Adventure"
+                            path="/services"
+                        />
                     </ul>
                 </div>
             </div>
