3a077caf1932a9ad933da98f920ca7aa30a2fd81
Jack Curtin <jackcurtin94@gmail.com>
2021-06-16 11:54:14 -0500

    added fontawesome icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
     return (
         <React.Fragment>
             <nav className="navbar">
@@ -9,6 +12,9 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         TRVL <i className="fab fa-typo3"></i>
                     </Link>
+                    <div className="menu-icon">
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                    </div>
                 </div>
             </nav>
         </React.Fragment>
