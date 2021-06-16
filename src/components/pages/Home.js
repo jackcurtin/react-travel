05c42d348b8c308c404eabb3b1fbb434efb0b958
Jack Curtin <jackcurtin94@gmail.com>
2021-06-16 15:29:54 -0500

    added Cards components

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import "../../App.css"
 import HeroSection from "../HeroSection";
+import Cards from "../Cards";
 import React from "react";
 
 function Home () {
     return (
         <React.Fragment>
             <HeroSection/>
+            <Cards/>
         </React.Fragment>
     )
 }
