8b3070c302715f1294f33d0e8e0f76a7f781303a
Jack Curtin <jackcurtin94@gmail.com>
2021-06-16 17:12:36 -0500

    added footer components with full css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import "../../App.css"
 import HeroSection from "../HeroSection";
 import Cards from "../Cards";
 import React from "react";
+import Footer from "../Footer";
 
 function Home () {
     return (
         <React.Fragment>
             <HeroSection/>
             <Cards/>
+            <Footer/>
         </React.Fragment>
     )
 }
